cd264ff617f5223dc7b134f6f7a1264c3a3cb565
Mahdi Nazari <mdi80nz@gmail.com>
2023-05-31 20:51:07 +0330

    last update for git ignore file

--- api/models.py
+++ api/models.py
@@ -17,8 +17,9 @@ class CommentProduct(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="category_image/",default="product_image/p1.jpg")
+    title = models.CharField(max_length=100)
+    image = models.ImageField(
+        upload_to="category_image/", default="product_image/p1.jpg")
 
 
 class Product(models.Model):
